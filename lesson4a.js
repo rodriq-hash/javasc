13e5c2531c6cb935aeec28e616b592dcdcf68532
rodriq-hash <rodriquendeda@gmail.com>
2026-03-02 13:52:29 +0300

    message1

---
+++ lesson4a.js
@@ -0,0 +1,41 @@
+// function with parameters
+function greeting(name) {
+    console.log("hello "+ name)
+}
+// call the function 
+greeting("Mike")
+greeting("Clarissa")
+// example 2
+function mycounty(county) {
+    console.log("my county is "+ county)
+}
+// call the function 
+mycounty("Nairobi")
+mycounty("Kirinyaga")
+mycounty("Meru")
+// examople 5
+function addition(num1,num2) {
+    answer=num1+num2
+    console.log("the sum is " + answer)
+}
+// call the function 
+addition(50,100)
+addition(800,700)
+addition(44,-160)
+addition(-90,-180)
+function subtract(num1,num2) {
+    answer=num1-num2
+    console.log("the difference is " + answer)
+}
+// call the function 
+subtract(600,800)
+subtract(140,55)
+subtract(20,90)
+function multiply(num1,num2) {
+    answer=num1*num2
+    console.log("the product is " + answer)
+}
+// call the function 
+multiply(104,77)
+multiply(55,10.2)
+multiply(33,88)
---
+++ lesson4b.js
@@ -0,0 +1,10 @@
+// anonymous functions
+const greeting=function(){
+    console.log("Hello World");
+}
+// call the function
+greeting();
+// self executing  functions 
+(function(){
+    console.log("immediately invoked functions");
+})();
