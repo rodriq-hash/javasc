2316e040ca4c786b067ab98695760abc45b5eea6
rodriq-hash <rodriquendeda@gmail.com>
2026-03-02 12:07:57 +0300

    messsage 4

---
+++ recaplesson3.js
@@ -0,0 +1,54 @@
+// function to add two numbers
+function addition (){
+    let num1=40
+    let num2=60
+    let answer=num1+num2
+    console.log("the sum is"+ answer)
+}
+// call the function 
+addition()
+// functio to subtract two numbers
+function subtraction() {
+    let num1=830
+    let num2=540
+    let answer=num1-num2
+    console.log("the difference is" + answer);
+}
+// call the function 
+subtraction()
+
+function multiply() {
+    let num1=25
+    let num2=56
+    let answer=num1*num2
+    console.log("the product is"+ answer);
+}
+// call the function 
+multiply()
+
+function divide() {
+  let num1=900
+  let num2=30
+  let answer=num1/num2
+  console.log("the quotient is "+ answer);
+}
+// call the function 
+divide()
+function simpleinterest () {
+    let time =3
+    let rate =5
+    let principal=8600
+    let answer=(rate*time*principal)/100
+    console.log("the simple interest is"+ answer)
+}
+// call the function 
+simpleinterest ()
+
+function bmi() {
+    let weight=80.4
+    let height=7.4
+    let answer=weight/(height**2)
+    console.log("the bmi is"+ answer)
+}
+// call the function 
+bmi()
